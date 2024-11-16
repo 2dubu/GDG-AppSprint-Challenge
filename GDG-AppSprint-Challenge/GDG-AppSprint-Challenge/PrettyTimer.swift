bbbb5582466bdabd6563b472a94173ab5044dcc6
Geonwoo Lee <2dubu.dev@gmail.com>
2024-11-16 14:35:26 +0900

    ✨ :: Connect actions

--- GDG-AppSprint-Challenge/GDG-AppSprint-Challenge/PrettyTimer.swift
+++ GDG-AppSprint-Challenge/GDG-AppSprint-Challenge/PrettyTimer.swift
@@ -11,8 +11,9 @@ import Combine
 struct PrettyTimer: View {
     @State private var progress: Double = 1
     @State private var remainingTime: Int = 60 * 5
-    @State private var formattedTime: String = "00:00"
+    @State private var formattedTime: String = "05:00"
     @State private var timer: AnyCancellable?
+    @State private var isRunning: Bool = false
     
     private let initialTime: Int = 60 * 5 // 5 min
     private var lineWidth: CGFloat = 8
@@ -26,8 +27,8 @@ struct PrettyTimer: View {
     
     private func startTimer() {
         formattedTime = formatTime(remainingTime)
+        isRunning = true
         
-        timer?.cancel()
         timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
             .sink { _ in
                 guard remainingTime > 0 else {
@@ -38,6 +39,15 @@ struct PrettyTimer: View {
                 progress = Double(remainingTime) / Double(initialTime)
                 formattedTime = formatTime(remainingTime)
             }
+    }
+    
+    private func resetTimer() {
+        remainingTime = 60 * 5
+        formattedTime = "05:00"
+        progress = 1
+        
+        isRunning = false
+        timer?.cancel()
     }
     
     var body: some View {
@@ -67,7 +77,7 @@ struct PrettyTimer: View {
                                     style: StrokeStyle(lineWidth: lineWidth, lineCap: .round)
                                 )
                                 .rotationEffect(Angle(degrees: -90))
-                                .animation(.easeInOut(duration: 0.25), value: progress)
+                                .animation(.easeInOut(duration: 0.1), value: progress)
                         }
                         .padding(18)
                         
@@ -84,6 +94,19 @@ struct PrettyTimer: View {
                 }
             }
             .frame(width: 275, height: 275)
+            
+            Spacer().frame(height: 26)
+            
+            // MARK: - Buttons
+            HStack(spacing: 12) {
+                SimpleButton(text: "Start", enable: !isRunning) {
+                    startTimer()
+                }
+                
+                SimpleButton(text: "Reset", enable: isRunning) {
+                    resetTimer()
+                }
+            }
         }
     }
 }

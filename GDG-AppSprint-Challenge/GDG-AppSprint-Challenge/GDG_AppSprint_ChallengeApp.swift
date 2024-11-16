fa11b7435837511752cbfeac349076f082e5be67
Geonwoo Lee <2dubu.dev@gmail.com>
2024-11-16 13:37:34 +0900

    🔧 :: basic setting

---
+++ GDG-AppSprint-Challenge/GDG-AppSprint-Challenge/GDG_AppSprint_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GDG_AppSprint_ChallengeApp.swift
+//  GDG-AppSprint-Challenge
+//
+//  Created by 이건우 on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct GDG_AppSprint_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PrettyTimer()
+        }
+    }
+}
---
+++ GDG-AppSprint-Challenge/GDG-AppSprint-Challenge/PrettyTimer.swift
@@ -0,0 +1,24 @@
+//
+//  PrettyTimer.swift
+//  GDG-AppSprint-Challenge
+//
+//  Created by 이건우 on 11/16/24.
+//
+
+import SwiftUI
+
+struct PrettyTimer: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    PrettyTimer()
+}
